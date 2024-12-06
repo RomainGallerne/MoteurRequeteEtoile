e5e1d45daf2300666fd81843ca2647e4209c3609
TheKing4012 <rick40709@gmail.com>
2024-12-06 16:10:40 +0100

    feat: update main by checking files type

--- java/RDF/nosql-engine-skeleton/src/main/java/qengine/benchmark/QueryBenchmark.java
+++ java/RDF/nosql-engine-skeleton/src/main/java/qengine/benchmark/QueryBenchmark.java
@@ -218,7 +218,7 @@ public class QueryBenchmark {
                     new QueryBenchmark(args[0],args[1], false);
                 }
             } else{
-                System.out.println("toto");
+                System.out.println("FIchiers mal formattés");
             }
         }
 
